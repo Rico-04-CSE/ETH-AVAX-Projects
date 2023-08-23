7e4c6e8172de5f685cf26d8caa15027f7e86eb4d
Rico-04-CSE <136056544+Rico-04-CSE@users.noreply.github.com>
2023-08-23 21:26:45 +0530

    Update Module 1 Project.sol

--- Module 1 Project.sol
+++ Module 1 Project.sol
@@ -6,15 +6,15 @@ contract Booking {
     mapping(address => bool) public hasBooked;
 
     function seatBooking() public payable {
-        require(!hasBooked[msg.sender], "Already Booked"); // Check if the sender's transaction is successful
+        require(!hasBooked[msg.sender], "Already Booked"); // Check if the sender's booking is successful
         
         totalBooking++;
         hasBooked[msg.sender] = true;
 
         if (totalBooking > 2) {
-            revert("Booking limit has exceeded"); //Revert if the transcation limit exceeded
+            revert("Booking limit has exceeded"); //Revert if the booking limit exceeded
         }
 
-        assert(totalBooking <= 2); //Make sure that the transactions does not exceed the given limit
+        assert(totalBooking <= 2); //Make sure that the booking does not exceed the given limit
     }
 }
